e1812ad8c71b4af3c51d823bd9d41af39489cf74
LeeTaeHoon97 <dlxogns1004@gmail.com>
2022-05-31 00:05:48 +0900

    Update loss.py

--- loss.py
+++ loss.py
@@ -2,5 +2,5 @@ import numpy as np
 import torch
 # ref) https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
 def Cross_Entropy_Loss(output, target ):
-	return torch.sum(torch.log(torch.exp(output) / torch.sum(torch.exp(output)))*target)
+	return (-1)*torch.sum(torch.log(torch.exp(output) / torch.sum(torch.exp(output)))*target)
 	
