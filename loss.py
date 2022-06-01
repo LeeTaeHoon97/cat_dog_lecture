e5cc1adaf8cb0e6311dd7e4cb99f4f1a8bc4bea0
LeeTaeHoon97 <dlxogns1004@gmail.com>
2022-06-02 01:03:35 +0900

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,3 +5,5 @@ def Binary_Cross_Entropy_Loss(output, target ):
 	
 	return torch.mean(-1* (target*torch.log(output)+(1-target)*torch.log(1-output)))
 	
+def CRELoss(output, target):
+	return torch.mean((-1.0)*torch.sum(torch.log(torch.exp(output) / torch.sum(torch.exp(output)))*target))
